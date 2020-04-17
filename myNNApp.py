2001142e707e4d4c3e9becb4a3fcf4c1bafd367c
Elsa Burren <63856139+ElsaBurren@users.noreply.github.com>
2020-04-17 23:45:49 +0800

    Add files via upload

--- myNNApp.py
+++ myNNApp.py
@@ -4,20 +4,21 @@ from myNN import MyHiddenLayer, MyOutputLayer
 import matplotlib.pyplot as plt
 
 def main():
+    # write your app here
+   
+   # example:
     numb_obs = 10
     numb_hidden = 2
     W = np.ones(numb_hidden)
-    my_hidden = MyHiddenLayer(W)
+    my_hidden = MyHiddenLayer(W,0)
     my_output = MyOutputLayer([-1, 1])
-   # X = np.array(np.linspace(-10, 10, numb_observations*w.shape[0]).reshape(numb_observations, w.shape[0]))
 
     x = np.linspace(-10,10,numb_obs)
     x = -1
     print(my_hidden.output(x))
     y = my_output.output(my_hidden.output(x))
     print(y)
-    plt.plot(y)
-    plt.show()
+    # end of example
 
 if __name__ == "__main__":
     main()
